c11dd5324e99405bea354b99f9937f5fe8de7780
bsametarman@outlook.com <bsametarman@outlook.com>
2025-10-09 16:39:00 +0300

    feat(configuration): add jwt config

--- src/Services/AlertService/Program.cs
+++ src/Services/AlertService/Program.cs
@@ -1,15 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using RateWatch.AlertService.Application.Services;
 using RateWatch.AlertService.Domain.Interfaces;
 using RateWatch.AlertService.Infrastructure.Data;
 using RateWatch.AlertService.Infrastructure.Repositories;
 using StackExchange.Redis;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AlertContext>(options =>
     options.UseNpgsql(connectionString));
+
+var appSettingsToken = builder.Configuration.GetSection("AppSettings:Token").Value;
+if (string.IsNullOrEmpty(appSettingsToken))
+    throw new Exception("AppSettings Token is not configured.");
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,6 +37,19 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
     return ConnectionMultiplexer.Connect(options);
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(appSettingsToken)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddAuthorization();
+
 builder.Services.AddScoped<IAlertRepository, AlertRepository>();
 builder.Services.AddScoped<IAlertService, AlertService>();
 
@@ -44,7 +64,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
