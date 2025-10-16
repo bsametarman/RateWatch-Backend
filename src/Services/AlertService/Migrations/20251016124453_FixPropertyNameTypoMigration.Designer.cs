768c04c83ecd99c543ead2785d4e884e8425c0b6
bsametarman@outlook.com <bsametarman@outlook.com>
2025-10-16 15:46:11 +0300

    feat: new migration for property name change

--- src/Services/AlertService/Migrations/20251016124453_FixPropertyNameTypoMigration.Designer.cs
+++ src/Services/AlertService/Migrations/20251016124453_FixPropertyNameTypoMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RateWatch.AlertService.Infrastructure.Data;
@@ -11,9 +12,11 @@ using RateWatch.AlertService.Infrastructure.Data;
 namespace RateWatch.AlertService.Migrations
 {
     [DbContext(typeof(AlertContext))]
-    partial class AlertContextModelSnapshot : ModelSnapshot
+    [Migration("20251016124453_FixPropertyNameTypoMigration")]
+    partial class FixPropertyNameTypoMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/AlertService/Migrations/20251016124453_FixPropertyNameTypoMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RateWatch.AlertService.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPropertyNameTypoMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Treshold",
+                table: "Alerts",
+                newName: "Threshold");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Threshold",
+                table: "Alerts",
+                newName: "Treshold");
+        }
+    }
+}
